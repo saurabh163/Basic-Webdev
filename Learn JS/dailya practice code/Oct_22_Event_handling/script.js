59bc0e129ae2dd1d6ec6b264d7cacdff12a07d0e
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-10-25 23:28:09 +0530

    cheak age

---
+++ Learn JS/dailya practice code/Oct_22_Event_handling/script.js
@@ -0,0 +1,147 @@
+
+// massage show //
+
+const btn = document.getElementById("btn");
+const msg = document.getElementById("msg");
+
+btn.addEventListener("click", function() {
+  msg.textContent = "Hello Bro I'm lernning event handling ";
+});
+
+
+
+
+// box color change //
+
+const box = document.getElementById("box");
+
+box.addEventListener("dblclick", function() {
+  box.style.backgroundColor = "yellow";
+});
+
+
+
+
+// mouse hover //
+
+const text = document.getElementById("text");
+
+text.addEventListener("mouseover", function() {
+  text.textContent = "laern mouse over event";
+});
+
+text.addEventListener("mouseout", function() {
+  text.textContent = "Hover on me";
+});
+
+
+
+// input text show /
+
+const input = document.getElementById("name");
+const show = document.getElementById("show");
+
+input.addEventListener("input", function() {
+  show.textContent = input.value;
+});
+
+
+
+/// press keybord key //
+
+const showKey = document.getElementById("showKey");
+
+document.addEventListener("keydown", function(event) {
+  showKey.textContent = "You pressed: " + event.key;
+});
+
+
+
+
+// form submit alert //
+
+const form = document.getElementById("myForm");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // form ke reload hone se rokta hai
+  alert("Form Submitted Successfully! ✅");
+});
+
+
+/// massge show //
+
+const checkbox = document.getElementById("agree");
+const Msg = document.getElementById("msg");
+
+checkbox.addEventListener("change", function() {
+  if (checkbox.checked) {
+    Msg.textContent = "✅ You agreed!";
+  } else {
+    Msg.textContent = "";
+  }
+});
+
+
+// paragraph hide show //
+
+const bTn = document.getElementById("toggleBtn");
+const tExt = document.getElementById("text");
+let visible = true;
+
+bTn.addEventListener("click", function() {
+  if (visible) {
+    tExt.style.display = "none";
+    bTn.textContent = "Show Text";
+    visible = false;
+  } else {
+    tExt.style.display = "block";
+    bTn.textContent = "Hide Text";
+    visible = true;
+  }
+});
+
+
+
+// counter //
+
+const btN = document.getElementById("countBtn");
+const count = document.getElementById("count");
+let num = 0;
+
+btN.addEventListener("click", function() {
+  num++;
+  count.textContent = num;
+});
+
+
+//coordinates show karna //
+
+const coords = document.getElementById("coords");
+
+document.addEventListener("mousemove", function(event) {
+  coords.textContent = `X: ${event.clientX}, Y: ${event.clientY}`;
+});
+
+
+
+// character count//
+
+const Input = document.getElementById("textInput");
+const Count = document.getElementById("count");
+
+Input.addEventListener("input", function() {
+  Count.textContent = "Characters: " + input.value.length;
+});
+
+
+
+// color badlna //
+
+const BTN = document.getElementById("colorBtn");
+const teXt = document.getElementById("text");
+
+BTN.addEventListener("click", function() {
+  const colors = ["red", "blue", "green", "purple", "orange"];
+  const random = Math.floor(Math.random() * colors.length);
+  teXt.style.color = colors[random];
+});
---
+++ Learn JS/oct 24 forms and/form1.js
@@ -0,0 +1,120 @@
+
+// check field empty //
+
+function checkEmpty() {
+  let name = document.getElementById("username").value;
+  if (name === "") {
+    document.getElementById("message").innerText = "Please enter your name!";
+    return false; // form will not submit 
+
+  } else {
+    document.getElementById("message").innerText = "Form submitted!";
+    return true;
+  }
+}
+
+
+
+
+// check mail 
+
+
+function validateEmail() {
+  let email = document.getElementById("email").value;
+  if (!email.includes("@")) {
+    document.getElementById("msg").innerText = "Invalid email!";
+    return false;
+  } else {
+    document.getElementById("msg").innerText = "Valid email!";
+    return true;
+  }
+}
+
+
+
+//  password length //
+ 
+function validatePassword() {
+  let password = document.getElementById("pass").value;
+  if (password.length < 6) {
+    document.getElementById("output").innerText = "Password too short!";
+    return false;
+  }
+  document.getElementById("output").innerText = "Password accepted!";
+  return true;
+}
+
+
+
+// conform password match //
+
+function checkMatch() {
+  let p1 = document.getElementById("p1").value;
+  let p2 = document.getElementById("p2").value;
+
+  if (p1 !== p2) {
+    document.getElementById("msg").innerText = "Passwords do not match";
+    return false;
+  }
+  document.getElementById("msg").innerText = "Passwords match ";
+  return true;
+}
+
+
+// number input validation //
+
+function checkNumber() {
+  let value = document.getElementById("num").value;
+  if (isNaN(value)) {
+    document.getElementById("result").innerText = "Only numbers allowed ";
+    return false;
+  } else {
+    document.getElementById("result").innerText = "Valid number ";
+    return true;
+  }
+}
+
+
+
+/// select option validation //
+
+function checkSelect() {
+  let city = document.getElementById("city").value;
+  if (city === "") {
+    document.getElementById("msg").innerText = "Please select a city";
+    return false;
+  }
+  document.getElementById("msg").innerText = "City selected: " + city;
+  return true;
+}
+
+
+
+// fields required check //
+
+function allCheck() {
+  let n = document.getElementById("name").value;
+  let e = document.getElementById("email").value;
+  let p = document.getElementById("password").value;
+
+  if (n === "" || e === "" || p === "") {
+    document.getElementById("alert").innerText = "All fields are required!";
+    return false;
+  }
+  document.getElementById("alert").innerText = "Form submitted successfully!";
+  return true;
+}
+
+
+
+// check age //
+
+function checkAge() {
+  let age = document.getElementById("age").value;
+  if (age < 18) {
+    document.getElementById("msg").innerText = "You must be 18 or older";
+    return false;
+  }
+  document.getElementById("msg").innerText = "Welcome to form and form validation";
+  return true;
+}
