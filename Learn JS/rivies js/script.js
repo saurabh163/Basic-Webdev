f6ffa9fb1a4185a6f8789088395bae55ed464f82
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-10-27 23:49:16 +0530

    if else

--- Learn JS/rivies js/script.js
+++ Learn JS/rivies js/script.js
@@ -8,3 +8,14 @@ if (num > 0) {
 } else {
   console.log("Number is Negative");
 }
+
+
+// problem 2 //
+
+let age = 16;
+
+if (age >= 18) {
+  console.log("You are an Adult");
+} else {
+  console.log("You are a Minor");
+}
