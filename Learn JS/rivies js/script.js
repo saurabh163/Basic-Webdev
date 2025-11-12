5141889a49206aab3ee7413940dea8b6b63c24f1
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2025-11-12 23:20:34 +0530

    || problem

--- Learn JS/rivies js/script.js
+++ Learn JS/rivies js/script.js
@@ -1713,3 +1713,26 @@ else {
   console.log("No Discount, Please Pay Full Amount");
   console.log("Total Fare");
 }
+
+// problem 137 //
+
+let ticketPrice = 250;
+let tickets = 10;
+
+let Total = ticketPrice * tickets;
+
+if (ticketPrice <= 0 || tickets <= 0) {
+  console.log("Invalid Input");
+}  
+
+else if (total > 2000) {
+  let discount = total * 0.10;
+  let finalBill = total - discount;
+  console.log("You got 10% Discount");
+  console.log("Final Bill:");
+} 
+
+else {
+  console.log("No Discount, Please Pay Full Amount");
+  console.log("Total Bill:");
+}
