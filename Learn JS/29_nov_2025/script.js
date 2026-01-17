3eac3a3fc04fd0c1597ddc5939c0d1dc70b4144b
Saurabh Lanjewar <lanjewarsaurbh@gmail.com>
2026-01-17 22:17:08 +0530

    add true false function example

--- Learn JS/29_nov_2025/script.js
+++ Learn JS/29_nov_2025/script.js
@@ -887,3 +887,16 @@ while (i <= 5) {
 }
  
 
+// problem 66 //
+
+function isPositive(num) {
+  if (num > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPositive(5));
+console.log(isPositive(-2));
+ 
